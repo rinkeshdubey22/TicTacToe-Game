61c2c4eb1d8b95a7c4b9737e5a102c0ae6d31142
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-15 09:58:49 +0530

    [Rinkesh] : add Added method to allow player

--- TicTocToe.java
+++ TicTocToe.java
@@ -1,13 +1,31 @@
 
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class TicTocToe {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicToeTac Game");
 		char [] Board = CreateBoard();
+		char userLetter = chooseLetter();
+		char computerLetter;
+		if(userLetter=='X') {
+			computerLetter = '0';
+		}
+		else {
+			computerLetter = 'X';
+		}
+		System.out.println("Computer Letter is: " + computerLetter+" User Letter is : " + userLetter);
 
 	}
+	public static char chooseLetter()
+	{
+        	char symbol;
+        	Scanner UserInput = new Scanner(System.in);
+        	System.out.print("Enter your symbol");
+        	symbol=UserInput.next().toUpperCase().charAt(0);
+        	return symbol;
+    	}
 		public static char [] CreateBoard() {
 			char[] Board = new char[10];
 			for (int i = 0; i < Board.length; i++) {
@@ -15,6 +33,10 @@ public class TicTocToe {
 			}
 		return Board;
 	}
+
+
+	
+
 
 
 }
