fbca21b7bea9963324429f103b159dd46c1b74fb
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-15 12:03:01 +0530

    [Rinkesh] : add Refactor the code

--- TicTocToe.java
+++ TicTocToe.java
@@ -1,62 +1,74 @@
-
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class TicTocToe {
+public class TicTocToe
+{
+    public static void main(String[] args)
+    {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to TicToeTac Game");
-		char [] Board = CreateBoard();
-		showBoard(Board);
-		PlayerMove(Board,userLetter);
-		showBoard(Board);
-		char userLetter = chooseLetter();
-		char computerLetter;
-		if(userLetter=='X') {
-			computerLetter = '0';
-		}
-		else {
-			computerLetter = 'X';
-		}
-		System.out.println("Computer Letter is: " + computerLetter+" User Letter is : " + userLetter);
-
-	}
-	public static char chooseLetter()
-	{
-        	char symbol;
-        	Scanner UserInput = new Scanner(System.in);
-        	System.out.print("Enter your symbol");
-        	symbol=UserInput.next().toUpperCase().charAt(0);
-        	return symbol;
-    	}
-	public static void showBoard(char[] Board) {
-        System.out.println("Currently the Board Looks Like ");
-        System.out.println(" ");
-        System.out.println("\n "+ Board[1] + " | "+ Board[2] + " | " + Board[3]);
-        System.out.println("-----------");
-     	System.out.println(" "+ Board[4] + " | " + Board[5] + " | " + Board[6]);
-        System.out.println("-----------");
-        System.out.println(" "+ Board[7] + " | " + Board[8] + " | " + Board[9]);
-        System.out.println(" ");
-    	}
-	public static void playerMove(char[] Board,char symbol) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the index from 1 to 9 to make the move");
-        int i=sc.nextInt();
-        if(Board[i]==' ')
+        char[] board = createBoard();
+        char UserLetter = chooseLetter();
+        char ComputerLetter;
+        if(UserLetter=='X')
         {
-            Board[i]=symbol;
+            ComputerLetter='O';
         }
-        else {
-               System.out.println("Index is not free");
-	}
-  }
-	public static char[] CreateBoard() {
-		char[] Board = new char[10];
-			for (int i = 1; i < Board.length; i++) {
-				Board[i] = ' ';
-			}
-		return Board;
-	}
+        else
+        {
+            ComputerLetter='X';
+
+        }
+        System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
+        showBoard(board);
+        int userMove = getUserMove(board);
+        showBoard(board);
+    }
+    public static int getUserMove(char[] board)
+    {
+        Scanner sc = new Scanner(System.in);
+        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
+        while (true)
+        {
+            System.out.println("What is your next move ? (1-9");
+            int index = sc.nextInt();
+            if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
+                return index;
+
+        }
+
+    }
+    private static boolean isSpaceFree(char[] board, int index)
+    {
+        return board[index] == ' ';
+    }
+    public static char chooseLetter()
+    {
+        char symbol;
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Enter your symbol");
+        symbol = UserInput.next().toUpperCase().charAt(0);
+        return symbol;
+    }
+
+    public static void showBoard(char[] board)
+    {
+        System.out.println("\n" + board[1] + "  |  "  + board[2] + "  |  " + board[3]);
+        System.out.println("--------------");
+        System.out.println(" " + board[4] + "  |  "  + board[5] + "  |  " + board[6]);
+        System.out.println("--------------");
+        System.out.println(" " + board[7] + "  |  "  +board[8] + "  |  " + board[9]);
+        System.out.println("--------------");
+    }
+    public static char[] createBoard()
+    {
+        char[] board = new char[10];
+        for(int index = 1; index < board.length; index++)
+        {
+            board[index] = ' ';
+        }
+        return  board;
+    }
 }
